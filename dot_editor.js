bbc922a9d8f7f474e615b8078028a7d5875c2e0f
bogyuni <q11343@Q11343-2PN1170200003.local>
2024-01-29 10:05:10 +0900

    add

--- dot_editor.js
+++ dot_editor.js
@@ -11,9 +11,8 @@ Coloris.setInstance('.coloris', {
   swatches: [ '#067bc2', '#84bcda', '#80e377', '#ecc30b', '#f37748', '#d56062' ]
 });
 
-const container = document.getElementById('baseContainer').innerHTML;
-const base = document.getElementById('container');
-
+const base = document.getElementById('baseContainer');
+const container = document.getElementById('container');
 const setValue = {
   baseWidth: 500,
   baseHeight: 500,
@@ -26,7 +25,6 @@ let dots = [];
 
 function createDot(e){
   const posX = e.offsetX;
-  console.log(posX);
   const posY = e.offsetY;
   const dotX = Math.floor(posX / setValue.dotWidth) * setValue.dotWidth;
   const dotY = Math.floor(posY / setValue.dotHeight) * setValue.dotHeight;
@@ -58,7 +56,7 @@ function createDot(e){
   // 내가 클릭한 대상이 container가 아니라, 기존에 찍은 dot를 클릭하면 dot 레이어를 기준으로 위치값을 잡기 때문에 도트 크기 이하의 숫자가 찍힘(10이하)
   // 그러면 container 첫번 째 도트인 0, 0 값을 반환함. 이후로는 중복 체크를 잘 하지만, 한번은 반드시 오류가 나게되어있음.
   // 그래서 이번엔 내가 클릭한 대상이 container 일 때만 도트를 찍도록 수정
-  if (e.target === base) {
+  if (e.target === container) {
     insertDot();
   } else{
     console.log('중복');
@@ -85,14 +83,14 @@ function designOn() {
   container.style = designBackground;
 }
 function designOff() {
-  container.style = ''
+  container.style = '';
 }
 
 function codeCopy() {
   
-  navigator.clipboard.writeText(container)
+  navigator.clipboard.writeText(base.innerHTML)
   .then(() => {
-    console.log('Copied to clipboard : ' + container);
+    console.log('Copied to clipboard : ' + base.innerHTML);
   })
   .catch(err => {
     console.log('Something went wrong', err);
